f00086ca6380035347d1eefd54aaa936ffd51c22
sh960440 <54958110+sh960440@users.noreply.github.com>
2021-02-11 05:14:58 -0500

    Timer

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public float timer = 60.0f;
+    [SerializeField] Text timerText;
+    private bool isTiming = false;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (isTiming)
+        {
+            timer -= Time.deltaTime;
+        }
+
+        timerText.text = "TIME: " + timer.ToString("0.00");
+        Debug.Log(timer);
+    }
+
+    public void StartTimer()
+    {
+        isTiming = true;
+    }
+
+    public void StopTiming()
+    {
+        isTiming = false;
+    }
+}
